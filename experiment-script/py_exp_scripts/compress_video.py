40a78fd767e900e6bd53313f5d77306e9066a6a7
cmydlarz <cmydlarz@nyu.edu>
2020-04-05 22:50:31 -0400

    ready to run script

--- experiment-script/py_exp_scripts/compress_video.py
+++ experiment-script/py_exp_scripts/compress_video.py
@@ -1,6 +1,7 @@
-import os
+import os, glob
 import subprocess
 import shlex
+from tqdm import tqdm
 
 
 def return_gstreamer_string(infile, bitrate_kbs, outpath):
@@ -9,22 +10,33 @@ def return_gstreamer_string(infile, bitrate_kbs, outpath):
 			'qtdemux ! ' \
 			'queue ! ' \
 			'h264parse ! ' \
-			'omxh264dec ! ' \
-			'omxh264enc bitrate=%d ! ' \
+			'nvv4l2decoder ! ' \
+			'nvv4l2h264enc bitrate=%d ! ' \
 			'h264parse ! ' \
 			'qtmux ! ' \
-			'filesink location=%s/out_%dkbps.mp4 -e' \
-			% (infile, bitrate_kbs * 1024, outpath, bitrate_kbs)
+			'filesink location=%s/%s_out_%dkbps.mp4 -e' \
+			% (infile, bitrate_kbs * 1024, outpath, os.path.basename(infile), bitrate_kbs)
 	print(gstr)
 	return gstr
 
 
-input_file = 'demo_video/demo.mp4'
-bitrate = 2000
+input_path = 'input_video'
+st_bitrate = 14500
+en_bitrate = 20
+bitrate_jump = 200
 output_root_path = 'output_video'
 
-gstr = return_gstreamer_string(input_file, bitrate, output_root_path)
-record_proc = subprocess.Popen(shlex.split(gstr), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-outs, errs = record_proc.communicate()
-print('Error: %s' % str(errs))
-print('Outputs: %s' % str(outs))
+# print(list(range(st_bitrate, en_bitrate, -bitrate_jump)))
+# print(len(list(range(st_bitrate, en_bitrate, -bitrate_jump))))
+
+
+for input_file in glob.glob(input_path + '/*.mp4'):
+	print(input_file)
+
+	for comp_bitrate in tqdm(list(range(st_bitrate, en_bitrate, -bitrate_jump))):
+		gstr = return_gstreamer_string(input_file, comp_bitrate, output_root_path)
+		record_proc = subprocess.Popen(shlex.split(gstr), stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+		outs, errs = record_proc.communicate()
+		print('Error: %s' % str(errs))
+		print('Outputs: %s' % str(outs))
+
